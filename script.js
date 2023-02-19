a8dfc3c6d89b2d00895852658ce02e3eef7d607f
MississippiBrenn <cararbrennan@gmail.com>
2023-02-19 13:58:12 -0800

    randomize button

--- script.js
+++ script.js
@@ -2,25 +2,40 @@ window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1')
     //built in canvas 2d api 
     const ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth * 0.8; 
-    canvas.height = window.innerHeight * 0.8; 
+    canvas.width = window.innerWidth; 
+    canvas.height = window.innerHeight; 
     //canvas settings
     ctx.fillStyle = 'green';
-    ctx.strokeStyle = 'yellow';
-    ctx.lineWidth = 30
-    ctx.lineCap='round';
+    ctx.strokeStyle = 'gold';
+    ctx.lineWidth = 10;
+    ctx.lineCap ='round';
+    ctx.shadowColor = 'rgba(0,0,0,0.7)';
+    ctx.shadowOffsetX = 10; 
+    ctx.shadowOffsetX = 5;
+    ctx.shadowBlur = 10; 
 
     //effect settings
-    let size = 200;   
-    let sides = 20;
-    let maxLevel = 3;
-    let spread = 0.5;
-    let branches = 2;
-    ctx.save();
-    ctx.translate(canvas.width/2,canvas.height/2);
-    ctx.scale(1,1);
-    ctx.rotate(0);
-    // ctx.fillRect(0,0, canvas.width, canvas.height);
+    let size = canvas.width <canvas.height ? canvas.width * 0.3 : canvas.height * 0.3;
+    //depth of fractal 
+    const maxLevel = 4;
+    const branches = 2;
+
+    // number of sides to form full circle  
+    let sides = Math.random() * 7 + 2;
+    //how much smaller the segmets are compared to the previous segment
+    let scale = Math.random() * 0.2 + 0.4;
+    //angle in radians between parent branches and their segments
+    //0 is cool 
+    let spread = Math.random() * 2.9 + 0.1;   
+    // hsl color declaration 0% is gray scale and 100% is full saturated 
+    let color = 'hsl('+Math.random() * 360 +', 100%, 50%)';
+     drawFractal();
+
+     
+    //controls 
+    const randomizeButton = document.getElementById('randomizeButton')
+
+
 
     function drawBranch(level){
         if(level > maxLevel) return; 
@@ -30,36 +45,51 @@ window.addEventListener('load', function(){
         ctx.stroke();
         for(let i =0; i < branches; i++){
             ctx.save();
-            ctx.translate((size/branches)*i,0)
+            ctx.translate(size-(size/branches)*i,0);
             ctx.rotate(spread);
-            ctx.scale(0.8,0.8); 
+            ctx.scale(scale,scale); 
             drawBranch(level+1);
             ctx.restore(); 
 
             ctx.save();
-            ctx.translate((size/branches)*i,0)
+            ctx.translate(size -(size/branches)*i,0);
             ctx.rotate(-spread);
-            ctx.scale(0.8,0.8); 
+            ctx.scale(scale,scale); 
             drawBranch(level+1);
             ctx.restore();
         }
 
     }
 
-    drawBranch(0);
 
 
-    // for(let i =0; i < sides; i++){
-    // ctx.beginPath();
-    // ctx.moveTo(0,0);
-    // ctx.lineTo(size,0);
-    // ctx.stroke();
-    // ctx.rotate((Math.PI * 2)/sides);
-    // ctx.scale(.95,.95);
-    // ctx.translate(30,50);
-    // }
-  
+    function drawFractal(){
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        ctx.save();
+        ctx.strokeStyle = color;
+        ctx.translate(canvas.width/2,canvas.height/2);
+        for(let i =0; i < sides; i++){
+            ctx.rotate((Math.PI *2)/sides);
+            drawBranch(0);
+            }
 
-    ctx.restore();
+        ctx.restore();
+    }
 
+    drawFractal();
+    
+    function randomizeFractal(){
+        // number of sides to form full circle  
+         sides = Math.random() * 7 + 2;
+        //how much smaller the segmets are compared to the previous segment
+         scale = Math.random() * 0.2 + 0.4;
+        //angle in radians between parent branches and their segments
+        //0 is cool 
+         spread = Math.random() * 2.9 + 0.1;   
+        // hsl color declaration 0% is gray scale and 100% is full saturated 
+         color = 'hsl('+Math.random() * 360 +', 100%, 50%)';
+         drawFractal();
+    }
+
+    randomizeButton.addEventListener('click', randomizeFractal);
 });
